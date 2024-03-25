6493239cab4ee5ce3547f8c562ffcaf14c678b06
KarabasBurak <117377125+KarabasBurak@users.noreply.github.com>
2024-03-25 22:49:52 +0300

    Add project files.

---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<T>where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();  // IQuaryable sorgulu bir şekilde tüm dataları getirmek için oluşturduk
+
+        IQueryable<T> Where(Expression<Func<T,bool>>predicate); // ToList() yapmadan liste gelmez.
+        Task AddAsync(T entity);
+        void Remove(T entity);
+        void Update(T entity);
+
+    }
+}
---
+++ SharedLibrary/Response.cs
@@ -0,0 +1,47 @@
+﻿using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    public class Response<T> where T : class
+    {
+        public T? Data { get; private set; }
+        public int StatusCode { get; private set; }
+        public ErrorDto? Error { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccesful { get; private set; } // Client'lara göstermek istemediğimiz bir property. Kendi iç yapımızda kullanacağız. API, Mini API gibi
+
+
+
+
+        // METODLAR
+        public static Response<T> Success(T data,int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccesful = true };
+        }
+
+        // İstek başaralı ama data dönmek istemediğimiz yerde bu metodu kullanacağız. Data=default yaptık, status code se yukarıdan gelecek.Silme ve güncelleme işlemlerinden sonra data dönmeye gerek yok. Yani sildiğin veya güncellediğin datayı tekrar dönme. Ekleme işleminde, eklediğin datayı geri dönmemiz lazım
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode, IsSuccesful = true };
+        }
+
+        public static Response<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccesful = false };
+        }
+
+        // Sadece bir tane hata aldığımızda bu metodu kullan
+        public static Response<T> Fail(string errorMessage,int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+            return new Response<T> { Error = errorDto,StatusCode = statusCode, IsSuccesful = false };
+        }
+    }
+}
